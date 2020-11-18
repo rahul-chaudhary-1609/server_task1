2e3e83535c8244f8b522f52bb0c786d33bc1b554
Rahul Chaudhary <rahulchaudhary99r@gmail.com>
2020-11-19 01:28:15 +0530

    UserInfo model and migration created: success

--- server/admin_app/models.py
+++ server/admin_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    mobile = models.CharField(max_length=10)
+    password = models.CharField(max_length=15)
